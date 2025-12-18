8ed6b1549766e34d9e334e196c2d2c03c6a0ce75
dashatrubavina <nazderry@gmail.com>
2025-12-18 20:37:10 +0300

    тестовые задачи

---
+++ lesson_2.py
@@ -0,0 +1,73 @@
+# Тест 1
+#pi = 3.1415926535 #создала переменную pi со значением
+
+#result = pi * 2 #создала переменную result с результатом умножения
+
+#print(result) #вывела значение переменной тип данных float
+
+# Тест 2
+
+#distance = 72
+
+#speed = 12
+
+#rapprochement = speed * 2
+
+#time = distance // rapprochement # время (// - выведет int, / - выведет float)
+
+#print(time)
+
+# Тест 3
+
+#quantity_1 = 25
+
+#quantity_2 = 40
+
+#quantity_3 = 55
+
+#total = quantity_1 + quantity_2 + quantity_3
+
+#print(total)
+
+#  Тест 4
+#
+# piece = 150
+#
+# piece_1 = piece - 35
+#
+# total = piece + piece_1
+#
+# print(total)
+
+# Тест 5
+
+#raspberry = 34
+
+#currant = raspberry * 2
+
+#difference = currant - raspberry
+
+
+
+#print(difference)
+
+
+# Тест 6
+#
+# second = 65 // 5
+# first = 4 * second
+#
+# print(f"Первая стопка: {first}")
+# print(f"Вторая стопка: {second}")
+#
+# print(f"Первая стопка: {first}\nВторая стопка: {second}")
+
+
+
+
+
+
+
+
+
+
